577addb5326a2d397369cc9aacf199cebc86ad5b
Elias Griffith <griffith.elias@gmail.com>
2017-01-10 19:42:25 -0500

    Possibly fix break in logic

--- 2016-17-Starstruck/behaviours.c
+++ 2016-17-Starstruck/behaviours.c
@@ -1,26 +1,34 @@
-#define currentPos SensorValue[clawManiple]
 #define wide 2350
 #define closed 1200
 typedef struct {
 	int tolerance;
 	int desiredPos;
+	int currentPos;
 	int speed;
 	int enabled;
+	int difference;
 } clawManipulator;
 bool clawMan = true;
 clawManipulator claw1;
 task clawPos() {
-	claw1.desiredPos = currentPos;
+	claw1.desiredPos = SensorValue[clawManiple];
 	while(true) {
+		if(vexRT[Btn7L] && vexRT[Btn8R]) {
+			clawMan = !clawMan;
+			claw = 0;
+		}
+		while(vexRT[Btn7L] & vexRT[Btn8R]) {}
+		claw1.currentPos = SensorValue[clawManiple];
+		claw1.difference = abs(claw1.desiredPos - claw1.currentPos);
 		if(clawMan) {
-			if(abs(claw1.desiredPos - currentPos) > claw1.tolerance) {
+			if(claw1.difference > claw1.tolerance) {
 				if(motor[clawL] == 0){
-					claw = ((claw1.desiredPos - currentPos > 0) ? claw1.speed : -claw1.speed);
+					claw = ((claw1.desiredPos - claw1.currentPos > 0) ? claw1.speed : -claw1.speed);
 				}
-			} else {
-				claw=0;
+				} else {
+				claw = 0;
 			}
-			wait1Msec(20);
+
 			if(vexRT[Btn7L] & vexRT[Btn8R]) {
 				clawMan = false;
 				claw = 0;
@@ -30,52 +38,57 @@ task clawPos() {
 			}
 		}
 		if(!clawMan) {
-			if(vexRT[Btn7L] == 1 & vexRT[Btn8R] == 1) {
-				clawMan = true;
-				while(vexRT[Btn7L] == 1 & vexRT[Btn8R] == 1)
-				{
-
-				}
+			switch(vexRT[Btn5D]) {
+			case 1:
+					claw = 63;
+				break;
+			case 0:
+				claw = 0;
+				break;
+			}
+			switch(vexRT[Btn5U]) {
+			case 1:
+				claw = -63;
+				break;
+			case 0:
+				claw = 0;
+				break;
 			}
 		}
+		wait1Msec(100);
 	}
 }
 
 task macroSelection() {
 	while(true) {
-		switch(vexRT[Btn5D]) {
-		case 1:
+		if(vexRT[Btn5D]) {
 			claw1.desiredPos = wide;
-			break;
-		case 0:
-			break;
+			while(vexRT[Btn5D]) {
+
+			}
 		}
-		switch(vexRT[Btn5U]) {
-		case 1:
+		if(vexRT[Btn5U]) {
 			claw1.desiredPos = closed;
-			break;
-		case 0:
-			break;
+			while(vexRT[Btn5U]) {
+
+			}
 		}
+		wait1Msec(100);
 	}
 }
 task incDecTolerance() {
 	while(true) {
-		switch(vexRT[Btn7U]) {
-		case 1:
-			claw1.tolerance += 5;
-			while(vexRT[Btn7U]) {
-
-			}
-			break;
+		if(vexRT[Btn7U]) {
+			claw1.tolerance +=5;
+			while(vexRT[Btn7U]) {}
 		}
-		switch(vexRT[Btn7D]) {
-		case 1:
+
+		if(vexRT[Btn7D]){
 			claw1.tolerance -= 5;
 			while(vexRT[Btn7D]) {
 
 			}
-			break;
 		}
+		wait1Msec(100);
 	}
 }
