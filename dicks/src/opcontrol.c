e0b38adfd7bce18d340f5d81046c94d20db008e5
Elias Griffith <shadowninja.cp@gmail.com>
2018-02-12 09:06:14 -0500

    Update opcontrol.c

--- dicks/src/opcontrol.c
+++ dicks/src/opcontrol.c
@@ -1,34 +1,6 @@
-/** @file opcontrol.c
- * @brief File for operator control code
- *
- * This file should contain the user operatorControl() function and any functions related to it.
- *
- * Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/
- *
- * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
- * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
- */
 
 #include "main.h"
 #include "string.h"
-/*
- * Runs the user operator control code. This function will be started in its own task with the
- * default priority and stack size whenever the robot is enabled via the Field Management System
- * or the VEX Competition Switch in the operator control mode. If the robot is disabled or
- * communications is lost, the operator control task will be stopped by the kernel. Re-enabling
- * the robot will restart the task, not resume it from where it left off.
- *
- * If no VEX Competition Switch or Field Management system is plugged in, the VEX Cortex will
- * run the operator control task. Be warned that this will also occur if the VEX Cortex is
- * tethered directly to a computer via the USB A to A cable without any VEX Joystick attached.
- *
- * Code running in this task can take almost any action, as the VEX Joystick is available and
- * the scheduler is operational. However, proper use of delay() or taskDelayUntil() is highly
- * recommended to give other tasks (including system tasks such as updating LCDs) time to run.
- *
- * This task should never exit; it should end with some kind of infinite loop, even if empty.
- */
 #define threshold(x) (abs(x)>24?x:0)
 
 #define mogoVal joystickGetAnalog('2','3')
@@ -88,10 +60,9 @@ void blueListen(char * message) {
     if (strcmp(message, "reset\r\n") == 0) {
         analogCalibrate(LINE_TRACKER_PORT);
         encoderReset(liftEnc);
-        imeReset(0);
         encoderReset(swingEnc);
         encoderReset(mogoEnc);
-      //gyroReset(gyro);
+      	gyroReset(gyROH);
         fprint("Reset Sensor\r\n", uart1);
 
 
@@ -134,10 +105,10 @@ void operatorControl() {
       motorSet(6, liftVal);
     }
 
-		motorSet(4, (drive + rotate)/downshift);
-    motorSet(7, (-drive + rotate)/downshift);
-		motorSet(8, (drive + rotate)/downshift);
-		motorSet(9, (-drive + rotate)/downshift);
+	motorSet(4, (drive + rotate)/downshift);
+    	motorSet(7, (-drive + rotate)/downshift);
+	motorSet(8, (drive + rotate)/downshift);
+	motorSet(9, (-drive + rotate)/downshift);
     !(analogRead(7) > 1234) || joystickGetDigital(1, 5, JOY_DOWN)?motorSet(10, mogoVal_actual):motorSet(10, abs(mogoVal_actual)/4);
     if (grabButton) {
        if(autoGrabTH != NULL) {
