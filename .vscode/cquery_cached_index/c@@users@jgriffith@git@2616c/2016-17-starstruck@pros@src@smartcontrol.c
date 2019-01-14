2919ad6e99e3fc9c53485d5d85065681d08c177b
jcgrif <jgriffith@jjsnack.com>
2019-01-14 14:17:51 -0500

    more cleanup

--- .vscode/cquery_cached_index/c@@users@jgriffith@git@2616c/2016-17-starstruck@pros@src@smartcontrol.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-int desiredPos;
-void autoLift(int angle) {
-  while(analogRead(aLiftPot) < desiredPos) {
-
-  }
-}
--- .vscode/cquery_cached_index/c@@users@jgriffith@git@2616c/2017-18-in_the_zone@src@drivemotor.cpp
+++
@@ -1,9 +0,0 @@
-#include <main.h>
-
-GeneralRobotics::DriveMotor::DriveMotor() {
-  this->port_number = 0;
-}
-
-GeneralRobotics::DriveMotor::DriveMotor(unsigned char channel) {
-  this->port_number = channel;
-}
